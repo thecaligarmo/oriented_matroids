a6c589beaf170fdae9cb6b1c55738464435b712f
lizzy <lizzyflight@outlook.com>
2023-07-04 10:42:52 +0100

    Removing # keys

--- oriented_matroids/__init__.py
+++ oriented_matroids/__init__.py
@@ -3,5 +3,5 @@ from .oriented_matroid import OrientedMatroid
 from .oriented_matroids_category import OrientedMatroids
 
 from sage.misc.lazy_import import lazy_import
-#lazy_import('sage.matroids.oriented_matroids',
-            #'oriented_matroids_catalog', 'oriented_matroids')
+lazy_import('sage.matroids.oriented_matroids',
+            'oriented_matroids_catalog', 'oriented_matroids')
