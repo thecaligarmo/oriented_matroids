a1944411a2c138d7358993f6178d355005077f38
lizzy <lizzyflight@outlook.com>
2023-07-05 09:56:44 +0100

    Removed importing from oriented_matroids_category

--- oriented_matroids/circuit_oriented_matroid.py
+++ oriented_matroids/circuit_oriented_matroid.py
@@ -21,7 +21,6 @@ AUTHORS:
 
 
 from sage.structure.parent import Parent
-from oriented_matroids.oriented_matroids_category import OrientedMatroids
 from oriented_matroids.signed_subset_element import SignedSubsetElement
 from oriented_matroids.oriented_matroid import OrientedMatroid
 
