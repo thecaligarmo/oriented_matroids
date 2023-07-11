63fc2065b0e9aba6d6772d7b055f095da96e067f
lizzy <lizzyflight@outlook.com>
2023-07-11 15:13:11 +0100

    Changing file name

---
+++ oriented_matroids/all.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from constructor import OrientedMatroid
+
+from sage.misc.lazy_import import lazy_import
+lazy_import('sage.matroids.oriented_matroids',
+            'oriented_matroids')
