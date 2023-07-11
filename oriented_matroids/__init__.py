e542480fbe7a19d6192eaf7a4c944ae8c1f12ff3
lizzyflight <136332529+lizzyflight@users.noreply.github.com>
2023-07-11 15:17:55 +0100

    Delete __init__.py

--- oriented_matroids/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from constructor import OrientedMatroid
-
-from sage.misc.lazy_import import lazy_import
-lazy_import('sage.matroids.oriented_matroids',
-            'oriented_matroids')
