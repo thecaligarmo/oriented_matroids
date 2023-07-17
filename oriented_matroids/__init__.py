3f56622e03d13b90def8b4c45287800d370db4f9
Aram Dermenjian <aram.dermenjian.math@gmail.com>
2023-07-17 12:30:29 +0100

    Revert "Fix init file, by removing"
    This reverts commit f4d2c5c804a24fae578b19f4cad50914bbab9914.

--- oriented_matroids/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from .oriented_matroid import OrientedMatroid
-from .oriented_matroids_category import OrientedMatroids
-
-# from sage.misc.lazy_import import lazy_import
-#lazy_import('sage.matroids.oriented_matroids',
-#            'oriented_matroids_catalog', 'oriented_matroids')
